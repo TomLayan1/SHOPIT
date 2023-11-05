20736c2394d14270c41150840e85bfa94dd02b9c
TomLayan1 <afolayantomisin1919@gmail.com>
2023-11-05 23:28:34 +0100

    cart saved in localStorage

--- scripts/cartscript/cart.js
+++ scripts/cartscript/cart.js
@@ -1,17 +1,24 @@
 import {shopitProduct} from '../../data/products.js';
 
 // export cart variable with the use of module
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-  }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'))
 
+if (!cart){
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }
+  ];
+}
+
+function saveToCart(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+};
 // export addToCart function with the use of module
 export function addToCart(productId) {
 
