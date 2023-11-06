1399e7bd55a26d1ad08861bfdb34d5bef47c16ea
TomLayan1 <afolayantomisin1919@gmail.com>
2023-11-06 15:48:28 +0100

    cart quantity made not to be reduced below 1 and increase above 10

--- scripts/cartscript/cart.js
+++ scripts/cartscript/cart.js
@@ -4,16 +4,7 @@ import {shopitProduct} from '../../data/products.js';
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart){
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1
-    }
-  ];
+  cart = [];
 }
 
 console.log(cart);
@@ -154,6 +145,10 @@ document.querySelectorAll(`.js-reduce-btn`).forEach((reduceButton) =>{
       if (cartItem.productId === productId){
         cartItem.quantity -= 1;
         itemQuantity.innerHTML = cartItem.quantity;
+        if (cartItem.quantity < 1) {
+          cartItem.quantity = 1;
+          itemQuantity.innerHTML = cartItem.quantity;
+        }
       }
       saveToCart();
     })
@@ -170,6 +165,10 @@ document.querySelectorAll(`.js-add-btn`).forEach((addButton) =>{
       if (cartItem.productId === productId){
         cartItem.quantity += 1;
         itemQuantity.innerHTML = cartItem.quantity;
+        if (cartItem.quantity > 10) {
+          cartItem.quantity = 10;
+          itemQuantity.innerHTML = cartItem.quantity;
+        }
       }
       saveToCart();
     })
